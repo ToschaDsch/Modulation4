61ae19cfc0c969519c1446819facb96e51e2a4e9
toscha3030 <aukmiit@gmail.com>
2022-03-31 16:55:23 +0200

    app

--- app/src/main/java/com/aukmiit/modulation4/MainActivity.kt
+++ app/src/main/java/com/aukmiit/modulation4/MainActivity.kt
@@ -3,12 +3,26 @@ package com.aukmiit.modulation4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aukmiit.modulation4.databinding.ActivityMainBinding
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var interAd: InterstitialAd? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        (application as AppMainState).showAdIfAvailable(this){
+
+        }
+
+        initAdMob()
+    }
+
+    private fun initAdMob() {
+        MobileAds.initialize(this)
+        val adRequest = AdRequest.Builder().build()
     }
 }
